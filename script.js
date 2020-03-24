bddb90d2a97a7367d5e61f03609370df806b9642
Regulusbojah <55148126+Regulusbojah@users.noreply.github.com>
2020-03-24 12:47:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+<script type= "text/javascript">
 
+var name = "Bolaji Joseph";
+var courses = new Array ("HTML","CSS","JavaScript");
+
+document.write("My name is : "+name+ "<br/> "); 
+document.writeln("I'm into these front-end courses : "+ courses +".<br/>");
+
+
+for(let i=0; i<=200 ;i++){
+
+    if (i%2==1){    //since number of courses is an odd number of #3 
+        document.write(i +"<br/>");
+    }
+} 
+
+</script>
